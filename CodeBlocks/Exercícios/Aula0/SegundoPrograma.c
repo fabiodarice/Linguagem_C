fe24a443925e979e6f41ae52c2ee1765ead97356
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-21 18:02:52 -0300

    Aula 0

---
+++ CodeBlocks/Exercícios/Aula0/SegundoPrograma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main() {
+    char nome[10];
+    printf("Qual e o seu nome? ");
+    scanf("%s", &nome);
+    printf("Muito prazer em te conhecer %s", nome);
+    system("pause");
+}
